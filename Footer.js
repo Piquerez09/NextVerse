4187c5f6d0a7bf9465c8a2308b4fe1aa72c8b603
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-12-05 16:19:59 -0300

    j

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>&copy; 2024 NextVerse. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
