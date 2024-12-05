706b6e44e1c2f51b8b82259f3a80db6c098a9944
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-12-05 16:30:38 -0300

    j

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Theme Toggle
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  document.body.dataset.theme =
+    document.body.dataset.theme === 'dark' ? 'light' : 'dark';
+});
+
 // Post Functionality
 const postButton = document.getElementById('post-button');
 const postInput = document.getElementById('post-input');
@@ -17,11 +24,4 @@ postButton.addEventListener('click', () => {
   } else {
     alert('Please write something before posting!');
   }
-});
-
-// Theme Toggle
-const themeToggle = document.getElementById('theme-toggle');
-themeToggle.addEventListener('click', () => {
-  document.body.dataset.theme = 
-    document.body.dataset.theme === 'dark' ? 'light' : 'dark';
 });
